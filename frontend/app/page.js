56d18319b88ae5eec57448000e4859be0d2697c0
sarahliu-mma <chih-hsuan.liu@mail.mcgill.ca>
2026-07-01 09:28:36 -0400

    update API URL to Railway

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   const [arrFilter, setArrFilter] = useState("Tous");
 
   useEffect(() => {
-    fetch("http://localhost:8000/events")
+    fetch("https://mtlverde-production.up.railway.app/events")
       .then((res) => res.json())
       .then((data) => setEvents(data));
   }, []);
