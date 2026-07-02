4e8ce5fbc759d54b4a4c89ca3dd8cba3ff5b800c
sarahliu-mma <chih-hsuan.liu@mail.mcgill.ca>
2026-07-02 17:26:39 -0400

    fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://mtlverde0.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
